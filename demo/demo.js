7ffaa31d6a615bc62c920c52913f2ac165e60dc6
Ashesh Goplani <ashesh.goplani96@gmail.com>
2020-07-30 11:58:06 +0500

    gg

--- demo/demo.js
+++ demo/demo.js
@@ -1,15 +1,41 @@
 const { EnapsoFileWatcher } = require('../index');
 
-EnapsoFileWatcher.add('dsd')
+EnapsoFileWatcher.add([
+    { path: './watchfile/ashesh.txt', id: '1232134' },
+    { path: './watchfile/dd.txt', id: '123' },
+    { path: './watchfile/check.txt', id: 'as213123' },
+    { path: './watchfile/ddddf.txt', id: 'as213123' }
+])
+    .then(async (res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log('Error', err);
+    });
+
+// EnapsoFileWatcher.remove([{ path: './watchfile/check.txt', id: 'as213123' }])
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log('Error', err);
+//     });
+
+// EnapsoFileWatcher.removeAll()
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log('Error', err);
+//     });
+EnapsoFileWatcher.getWatched()
     .then((res) => {
         console.log(res);
     })
     .catch((err) => {
-        console.log('here in cathc err', err);
+        console.log('Error', err);
     });
 
-// EnapsoFileWatcher.on('fileChanged', function (message) {
-//     console.log(message);
-// });
-
-// console.log(EnapsoFileWatcher);
+EnapsoFileWatcher.on('fileChanged', function (message) {
+    console.log(message);
+});
