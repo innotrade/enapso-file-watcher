3015db27b1d4c2d0947340d9bf108ddb120b5d94
myasir92 <yasir7682@gmail.com>
2020-11-03 15:34:04 +0500

    new changes

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,7 @@
+// Innotrade Enapso File Watcher
+// (C) Copyright 2019 Innotrade GmbH, Herzogenrath, NRW, Germany
+// Author(s):  Alexander Schulze, Ashesh Goplani and Muhammad Yasir
+// Demo
 const { EnapsoFileWatcher } = require('../index');
 
 async function test() {
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Innotrade Enapso File Watcher
+// (C) Copyright 2019 Innotrade GmbH, Herzogenrath, NRW, Germany
+// Author(s):  Alexander Schulze, Ashesh Goplani and Muhammad Yasir
+
 const EnapsoFileWatcher = require('./lib/enapso-file-watcher');
 
 module.exports = {
